4370477bb7e48769b0bd7f7a49618ecf3417aaa3
Lele Chen <lelechen@fb.com>
2021-04-26 10:38:15 -0400

    add facescape training

--- data/custom_dataset_data_loader.py
+++ data/custom_dataset_data_loader.py
@@ -4,8 +4,8 @@ from data.base_data_loader import BaseDataLoader
 
 def CreateDataset(opt):
     dataset = None
-    from data.aligned_dataset import AlignedDataset
-    dataset = AlignedDataset()
+    from data.facescapeDataset import FacescapeDataset
+    dataset = FacescapeDataset()
 
     print("dataset [%s] was created" % (dataset.name()))
     dataset.initialize(opt)
