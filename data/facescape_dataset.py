9167cb0c1a1d737c076c1f828435b00817dde731
Lele Chen <lelechen@fb.com>
2021-04-26 10:28:57 -0400

    add facescape training

---
+++ data/facescape_dataset.py
@@ -0,0 +1,57 @@
+import os.path
+from data.base_dataset import BaseDataset, get_params, get_transform, normalize
+from data.image_folder import make_dataset
+from PIL import Image
+
+class FacescapeDataset(BaseDataset):
+    def initialize(self, opt):
+        self.opt = opt
+        self.root = opt.dataroot    
+
+        ### input A (renderred image)
+        self.dir_A = os.path.join(opt.dataroot, "fsmview_renderings")
+
+        ### input B (real images)
+        self.dir_B = os.path.join(opt.dataroot, "fsmview_images")
+
+        self.mode = opt.isTrain
+
+        if self.mode=='train':
+            _file = open(os.path.join(opt.dataroot, "lists/train.pkl"), "rb")
+        elif self.mode =='test':
+            _file = open(os.path.join(opt.dataroot, "lists/test.pkl"), "rb")
+        elif self.mode =='demo' :
+            _file = open(os.path.join(opt.dataroot, "lists/demo.pkl"), "rb")
+        self.data_list = pickle.load(_file)
+        _file.close()
+
+        
+    def __getitem__(self, index):        
+        ### input A (renderred image)
+        A_path = os.path.join( self.dir_A , data_list[index] )   
+        print (A_path)          
+        A = Image.open(A_path).convert('RGB')   
+        params = get_params(self.opt, A.size)
+        
+        transform_A = get_transform(self.opt, params)      
+        A_tensor = transform_A(A)
+
+
+        B_tensor = 0
+        ### input B (real images)
+        B_path = os.path.join( self.dir_B , data_list[index] )   
+        print (B_path)       
+        B = Image.open(B_path).convert('RGB')
+        transform_B = get_transform(self.opt, params)      
+        B_tensor = transform_B(B)
+
+     
+        input_dict = { 'renderred_image':A_tensor, 'image': B_tensor, 'path': A_path}
+
+        return input_dict
+
+    def __len__(self):
+        return len(self.data_list) // self.opt.batchSize * self.opt.batchSize
+
+    def name(self):
+        return 'FacescapeDataset'
