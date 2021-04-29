cc2e9c19063b046c256e2e083c7477a6fca22623
Lele Chen <lelechen@fb.com>
2021-04-29 11:09:10 -0400

    g

--- models/pix2pixHD_facescape.py
+++ models/pix2pixHD_facescape.py
@@ -116,7 +116,7 @@ class Pix2PixHDModel(BaseModel):
 
         # Fake Generation
         if eye_parsing is not None:
-            cat_input = torch.cat([renderred_image.cuda(), eye_parsing.cuda(),dim = 1])
+            cat_input = torch.cat((renderred_image.cuda(), eye_parsing.cuda()),dim = 1])
             
         else:
             cat_input = renderred_image.cuda()
@@ -168,7 +168,7 @@ class Pix2PixHDModel(BaseModel):
         # Fake Generation
         if eye_parsing is not None:
             eye_parsing = Variable(eye_parsing.data.cuda())
-            cat_input = torch.cat([renderred_image, eye_parsing,dim = 1])
+            cat_input = torch.cat((renderred_image, eye_parsing),dim = 1])
             
         else:
             cat_input = renderred_image
