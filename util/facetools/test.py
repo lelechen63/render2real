e9e17824eb7cf2103928549c5cc5dfd13e511a28
Lele Chen <lelechen@fb.com>
2021-04-29 21:17:12 -0400

    g

--- util/facetools/test.py
+++ util/facetools/test.py
@@ -23,6 +23,6 @@ facenet.eval()
 img_path = '/raid/celong/FaceScape/ffhq_aligned_img/1/1_neutral/1.jpg'  
 image = Image.open(img_path)
 # landmark = np.load(img_path.replace('ffhq_aligned_img', 'fsmview_landmarks')[:-3] + 'npy').transpose(1,0)[:,::-1]
-res = parsing(img_path)
+res = parsing(img_path, facenet, idet)
 print (res.shape)
 vis_parsing_maps(image, res, save_parsing_path='9_parsing.jpg', save_vis_path = 'imgs/9_vis.jpg' )
