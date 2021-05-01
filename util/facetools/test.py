a21e53d8c025b0481dc0bd3247e1268d0f823356
Lele Chen <lelechen@fb.com>
2021-05-01 18:21:48 -0400

    g

--- util/facetools/test.py
+++ util/facetools/test.py
@@ -42,9 +42,9 @@ def get_parsing_batch( ids ):
             # parsing_path = img_path.replace('ffhq_aligned_img', 'fsmview_landmarks')[:-4] +'_parsing.png'
             # vis_parsing_maps(image, res, save_parsing_path=parsing_path, save_vis_path ='/raid/celong/FaceScape/tmp/tmp2/' + id_p +'_' + motion_p +'_' + '33.png' ) 
             # print (gg)
-            for valid_f in range( len ( os.validface_list( current_p1 ))):
+            for valid_f in range( len ( os.listdir( current_p1 ))):
             # for valid_f in valid_idxs:
-                img_path = os.path.join( current_p1, valid_f + '.jpg')
+                img_path = os.path.join( current_p1, str(valid_f) + '.jpg')
                 # if os.path.exists(img_path[:-4] +'_mask.png'):
                 #     print (img_path, '-----')
                 #     continue
@@ -53,7 +53,7 @@ def get_parsing_batch( ids ):
                 try:
                     image = Image.open(img_path)
                     res = parsing(image, facenet, idet, img_path[:-4] +'_mask.png')
-                    vis_parsing_maps(image, res, save_parsing_path=parsing_path, save_vis_path ='/raid/celong/FaceScape/tmp/tmp2/' + id_p +'_' + motion_p +'_' +valid_f +'.png' ) 
+                    vis_parsing_maps(image, res, save_parsing_path=parsing_path)#, save_vis_path ='/raid/celong/FaceScape/tmp/tmp2/' + id_p +'_' + motion_p +'_' +str(valid_f_ +'.png' ) 
                 
                 except:
                     print ('**********')
