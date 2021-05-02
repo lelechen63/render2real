409e0bda03c150433911358389f21832c337fa17
Lele Chen <lelechen@fb.com>
2021-05-01 22:53:30 -0400

    g

--- util/facetools/test.py
+++ util/facetools/test.py
@@ -11,6 +11,30 @@ import torch
 os.environ["CUDA_VISIBLE_DEVICES"]="0"
 import time, threading
 
+
+expressions = {
+    1: "1_neutral",
+    2: "2_smile",
+    3: "3_mouth_stretch",
+    4: "4_anger",
+    5: "5_jaw_left",
+    6: "6_jaw_right",
+    7: "7_jaw_forward",
+    8: "8_mouth_left",
+    9: "9_mouth_right",
+    10: "10_dimpler",
+    11: "11_chin_raiser",
+    12: "12_lip_puckerer",
+    13: "13_lip_funneler",
+    14: "14_sadness",
+    15: "15_lip_roll",
+    16: "16_grin",
+    17: "17_cheek_blowing",
+    18: "18_eye_closed",
+    19: "19_brow_raiser",
+    20: "20_brow_lower"
+}
+
 fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D, flip_input=False)
 idet = IrisDetector()
 idet.set_detector(fa)
@@ -30,7 +54,8 @@ total_ids.sort()
 def get_parsing_batch( ids ):
     for id_p in ids:
         current_p = os.path.join( base_p , id_p)
-        for motion_p in os.listdir(current_p):
+        for k in range(len(expressions)):
+            motion_p = expressions[k]
             current_p1 = os.path.join( current_p , motion_p)
             # valid_idxs = valid_all[id_p +'__' + motion_p]
             # img_path = '/raid/celong/FaceScape/ffhq_aligned_img/1/1_neutral/1.jpg'  
