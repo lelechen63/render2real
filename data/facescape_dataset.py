eb738e91149d19e3464627132e4eb249b4b630c7
Lele Chen <lelechen@fb.com>
2021-05-03 11:39:45 -0400

    g

--- data/facescape_dataset.py
+++ data/facescape_dataset.py
@@ -3,7 +3,7 @@ from data.base_dataset import BaseDataset, get_params, get_transform, normalize
 from data.image_folder import make_dataset
 from PIL import Image, ImageChops
 import pickle 
-
+import cv2
 class FacescapeDataset(BaseDataset):
     def initialize(self, opt):
         self.opt = opt
