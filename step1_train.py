0823b5854e20d0d0ee45dfc423883949916cd9df
Lele Chen <lelechen@fb.com>
2021-05-09 17:25:32 -0400

    g

--- step1_train.py
+++ step1_train.py
@@ -101,7 +101,7 @@ for epoch in range(start_epoch, opt.niter + opt.niter_decay + 1):
             #call(["nvidia-smi", "--format=csv", "--query-gpu=memory.used,memory.free"]) 
 
         ### display output images
-        save_fake = True
+        # save_fake = True
         if save_fake:
             visuals = OrderedDict([ ('image', util.tensor2im(data['image'][0])),
                                     ('pair_image', util.tensor2im(data['pair_image'][0])),
