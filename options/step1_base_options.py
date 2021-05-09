f20a51b38b3fcd69899863af173fdc3964911d09
Lele Chen <lelechen@fb.com>
2021-05-09 15:14:11 -0400

    g

--- options/step1_base_options.py
+++ options/step1_base_options.py
@@ -45,7 +45,7 @@ class BaseOptions():
         self.parser.add_argument('--tf_log', action='store_true', help='if specified, use tensorboard logging. Requires tensorflow installed')
 
         # for generator
-        self.parser.add_argument('--netG', type=str, default='global', help='selects model to use for netG')
+        self.parser.add_argument('--netG', type=str, default='disent', help='selects model to use for netG')
         self.parser.add_argument('--ngf', type=int, default=64, help='# of gen filters in first conv layer')
         self.parser.add_argument('--n_downsample_global', type=int, default=5, help='number of downsampling layers in netG') 
         self.parser.add_argument('--n_blocks_global', type=int, default=9, help='number of residual blocks in the global generator network')
