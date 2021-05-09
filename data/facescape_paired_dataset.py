a7dbd283627cd68288770b0b7b68a2fa09eb7419
Lele Chen <lelechen@fb.com>
2021-05-09 18:20:11 -0400

    g

--- data/facescape_paired_dataset.py
+++ data/facescape_paired_dataset.py
@@ -158,8 +158,6 @@ class FacescapeDirDataset(BaseDataset):
         params = get_params(self.opt, B.size)
         transform = get_transform(self.opt, params)      
         B_tensor = transform(B)
-        print (viewpoint)
-        print(ggg)
         viewpoint = np.asarray(viewpoint)
 
         # B_tensor =1
