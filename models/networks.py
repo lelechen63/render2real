8f85a029b426f2d80cf78e3d9c59133fb10934d3
Lele Chen <lelechen@fb.com>
2021-05-10 19:32:40 -0400

    g

--- models/networks.py
+++ models/networks.py
@@ -565,9 +565,9 @@ class DisentEncoderDecoder2(nn.Module):
 
         A_encoded = self.resblocks(A_encoded)
         print(A_encoded.shape)
-        A_encoded = self.pool(A_encoded)
-        print(A_encoded.shape)
-        A_encoded = A_encoded.view(A_encoded.shape[0], -1)
+        # A_encoded = self.pool(A_encoded)
+        # print(A_encoded.shape)
+        # A_encoded = A_encoded.view(A_encoded.shape[0], -1)
 
         # A_identity_code = self.identity_enc(A_encoded)
         # A_expression_code = self.expression_enc(A_encoded)
@@ -577,9 +577,9 @@ class DisentEncoderDecoder2(nn.Module):
         # A_id_fea = self.identity_dec(A_identity_code)
         # A_feature = torch.cat([A_exp_fea, A_id_fea, A_view_fea], axis = 1)
         # A_code = self.code_dec(A_feature)
-        A_code = A_encoded.unsqueeze(2).unsqueeze(3).repeat(1, 1, 8,8) # not sure 
+        # A_code = A_encoded.unsqueeze(2).unsqueeze(3).repeat(1, 1, 8,8) # not sure 
 
-        A_decoded = self.decoder(A_code)
+        A_decoded = self.decoder(A_encoded)
         recons_A = self.output_layer(A_decoded)
 
         return_list.append( recons_A)
