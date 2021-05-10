370385b62cb540e86e5b9107bd88371d74327d50
Lele Chen <lelechen@fb.com>
2021-05-09 22:51:03 -0400

    g

--- step1_train.py
+++ step1_train.py
@@ -79,7 +79,7 @@ for epoch in range(start_epoch, opt.niter + opt.niter_decay + 1):
         loss_dict = dict(zip(model.module.loss_names, losses))
 
         # calculate final loss scalar
-        loss_pix = (loss_dict['A_pix'] + loss_dict['B_pix'] + loss_dict.get('mis_pix',0) ) 
+        loss_pix = (loss_dict['A_pix'] + loss_dict.get('B_pix',0) + loss_dict.get('mis_pix',0) ) 
         loss_vgg = loss_dict['A_vgg'] + loss_dict.get('B_vgg',0) + loss_dict.get('mis_vgg',0)
 
         loss_G = loss_pix + loss_vgg
