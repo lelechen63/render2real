013f598d67e3088299a057c931cc7725d7d94bc5
Lele Chen <lelechen@fb.com>
2021-05-11 18:22:31 -0400

    g

--- data/facescape_paired_dataset.py
+++ data/facescape_paired_dataset.py
@@ -99,7 +99,7 @@ class FacescapeDirDataset(BaseDataset):
         f  = open(json_path , 'r')
         params = json.load(f)
         print (params.keys())
-        print ('%s_Rt' % tmp[:-4])
+        print ('%s_Rt' % [-1][:-4])
         print ('++++')
         Rt = np.array(params['%s_Rt' % tmp[:-4]])
         print (Rt.shape, '+++++')
