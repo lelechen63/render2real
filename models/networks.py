ee94f685556e224dfa073ccc91ad57e92cde7edc
Lele Chen <lelechen@fb.com>
2021-05-10 22:15:51 -0400

    g

--- models/networks.py
+++ models/networks.py
@@ -464,10 +464,19 @@ class DisentEncoderDecoder(nn.Module):
         print (map_type.shape)
 
         print (A_exp_fea.shape)
-        if map_type == 0:
-            Aexp_Bid_fea = torch.cat([A_exp_fea, B_id_fea, A_view_fea], axis = 1)
-        else:
-            Aexp_Bid_fea = torch.cat([A_exp_fea, B_id_fea, B_view_fea], axis = 1)
+        Aexp_Bid_fea =[]
+        for i in range(map_type.shape[0]):
+            if map_type[i] == 0:
+        
+                Aexp_Bid_fea.append( torch.cat([A_exp_fea[i], B_id_fea[i], A_view_fea[i]], axis = 1) )
+                Bexp_Aid_fea.append( torch.cat([B_exp_fea[i], A_id_fea[i], B_view_fea[i]], axis = 1) )
+
+            else:
+                Aexp_Bid_fea.append( torch.cat([A_exp_fea[i], B_id_fea[i], B_view_fea[i]], axis = 1) )
+                Bexp_Aid_fea.append( torch.cat([B_exp_fea[i], A_id_fea[i], A_view_fea[i]], axis = 1) )
+
+        Aexp_Bid_fea = torch.stack(Aexp_Bid_fea, dim = 1)
+        Bexp_Aid_fea = torch.stack(Bexp_Aid_fea, dim = 1)
 
         Aexp_Bid_code = self.code_dec(Aexp_Bid_fea)
         Aexp_Bid_code = Aexp_Bid_code.unsqueeze(2).unsqueeze(3).repeat(1, 1, 2,2) # not sure 
@@ -475,10 +484,6 @@ class DisentEncoderDecoder(nn.Module):
         recons_Aexp_Bid = self.output_layer(Aexp_Bid_decoded)
 
         return_list.append( recons_Aexp_Bid)
-        if map_type == 0:
-            Bexp_Aid_fea = torch.cat([B_exp_fea, A_id_fea, B_view_fea], axis = 1)
-        else:
-            Bexp_Aid_fea = torch.cat([B_exp_fea, A_id_fea, A_view_fea], axis = 1)
 
         Bexp_Aid_code = self.code_dec(Bexp_Aid_fea)
         Bexp_Aid_code = Bexp_Aid_code.unsqueeze(2).unsqueeze(3).repeat(1, 1, 2,2) # not sure 
