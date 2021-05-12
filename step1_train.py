fc3e59c9ff92b3f577216403c5cf3aa066442367
Lele Chen <lelechen@fb.com>
2021-05-11 20:53:52 -0400

    g

--- step1_train.py
+++ step1_train.py
@@ -104,10 +104,10 @@ for epoch in range(start_epoch, opt.niter + opt.niter_decay + 1):
         # save_fake = True
         if save_fake:
             A_img = util.tensor2im(data['image'][0])
-            A_img = cv2.putText(A_img, data['A_path'][0], (50,50), fcv2.FONT_HERSHEY_SIMPLEXont, 
+            A_img = cv2.putText(A_img, data['A_path'][0], (50,50), cv2.FONT_HERSHEY_SIMPLEX, 
                    1, (255, 0, 0), 2, cv2.LINE_AA)
             B_img = util.tensor2im(data['pair_image'][0])
-            B_img = cv2.putText(B_img, data['B_path'][0], (50,50), fcv2.FONT_HERSHEY_SIMPLEXont, 
+            B_img = cv2.putText(B_img, data['B_path'][0], (50,50), cv2.FONT_HERSHEY_SIMPLEX, 
                    1, (255, 0, 0), 2, cv2.LINE_AA)
 
             visuals = OrderedDict([
