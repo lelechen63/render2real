9f4d20840e3408983e24597ea61e1c4db8e0bded
Lele Chen <lelechen@fb.com>
2021-05-11 22:58:33 -0400

    g

--- util/visualizer.py
+++ util/visualizer.py
@@ -37,8 +37,7 @@ class Visualizer():
     def display_current_results(self, visuals, epoch, step):
         if self.tf_log: # show images in tensorboard output
             img_summaries = []
-            for label, image_numpy in visuals.items():
-                     
+            for label, image_numpy in visuals.items(): 
                 # Write the image to a string
                 try:
                     s = StringIO()
