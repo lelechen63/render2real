8f3f22688bda6c0cccfc9b0b39e1ea557c2e33d2
Lele Chen <lelechen@fb.com>
2021-05-13 17:09:50 -0400

    g

--- models/networks.py
+++ models/networks.py
@@ -64,7 +64,7 @@ def define_Dis_EncoderDecoder(linearity, input_nc, code_n,encoder_fc_n, ngf, net
 def define_TexMesh_EncoderDecoder(linearity, input_nc, code_n,encoder_fc_n, ngf, netG, n_downsample_global=5, n_blocks_global=9, 
              n_blocks_local=3, norm='instance', gpu_ids=[]):    
     norm_layer = get_norm_layer(norm_type=norm)     
-    if netG == 'textmesh_dis':    
+    if netG == 'disent':    
         encoderdecoder = TexMeshEncoderDecoder(linearity, input_nc, code_n,encoder_fc_n, ngf, n_downsample_global, n_blocks_global)       
     
     else:
