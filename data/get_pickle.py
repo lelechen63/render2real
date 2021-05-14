68fe12d47779de1506e51117751adc2b2b2d9488
Lele Chen <lelechen@fb.com>
2021-05-14 13:14:43 -0400

    g

--- data/get_pickle.py
+++ data/get_pickle.py
@@ -115,21 +115,20 @@ def get_texmesh_pickle():
                 all_motions.append(f[:-4])
         random.shuffle(all_motions)
         for k, motion_p in enumerate(all_motions):
-            # try:
+            try:
                 tex_path = os.path.join(current_p, motion_p + '.jpg')
                 mesh_path = os.path.join(current_p, motion_p + '.obj')
                 tex = Image.open(tex_path)
                 om_mesh = openmesh.read_trimesh(mesh_path)
                 A_vertices = np.array(om_mesh.points())
-                print (A_vertices.shape)
                 if A_vertices.shape[0] == 0:
                     continue
                 if k < 17:
                     train_list.append( os.path.join( id_p , 'models_reg', motion_p) )
                 else:
                     test_list.append( os.path.join( id_p , 'models_reg',  motion_p) )
-            # except:
-            #     continue
+            except:
+                continue
     print (test_list[:10])
     print (len(train_list), len(test_list))
 
