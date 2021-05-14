bc9f22b910ac8b6f6dfadd503227e77884270bc7
Lele Chen <lelechen@fb.com>
2021-05-14 15:56:07 -0400

    g

--- data/tmp.py
+++ data/tmp.py
@@ -5,10 +5,10 @@ import cv2
 import numpy as np
 tex_path = '/raid/celong/FaceScape/textured_meshes/1/models_reg/16_grin.jpg'
 # mesh 
-# tex = Image.open(tex_path).convert('RGB')#.resize(self.img_size)
-# tex  = np.array(tex ) 
+tex = Image.open(tex_path)#.convert('RGB')#.resize(self.img_size)
+tex  = np.array(tex ) 
 # tex = cv2.cvtColor(tex, cv2.COLOR_RGB2BGR)
-tex = cv2.imread(tex_path)
+# tex = cv2.imread(tex_path)
 # tex = cv2.resize(tex, self.img_size, interpolation = cv2.INTER_AREA)
 # tex = tex * self.facial_seg
 cv2.imwrite('./gg.png', tex)
