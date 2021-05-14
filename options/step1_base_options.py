c3947bfa5c7bba2261efd04decf9a3313e4b207d
Lele Chen <lelechen@fb.com>
2021-05-14 12:41:45 -0400

    g

--- options/step1_base_options.py
+++ options/step1_base_options.py
@@ -65,7 +65,7 @@ class BaseOptions():
             self.initialize()
         self.opt = self.parser.parse_args()
         self.opt.isTrain = self.isTrain   # train or test
-        self.opt.model_name = self.opt.name
+        self.opt.model = self.opt.name
         str_ids = self.opt.gpu_ids.split(',')
         self.opt.gpu_ids = []
         for str_id in str_ids:
