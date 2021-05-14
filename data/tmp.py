e25b450e9dbe9e0cd94d9be93ccd26753b8fbd38
Lele Chen <lelechen@fb.com>
2021-05-14 15:59:14 -0400

    g

--- data/tmp.py
+++ data/tmp.py
@@ -3,10 +3,15 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 import PIL
 import cv2
 import numpy as np
+
+facial_seg = Image.open("./predef/facial_mask_v10.png")
+facial_seg  = np.array(facial_seg ) 
+print (np.unique(facial_seg))
+
 tex_path = '/raid/celong/FaceScape/textured_meshes/1/models_reg/16_grin.jpg'
 # mesh 
 tex = Image.open(tex_path).convert('RGB')#.resize(self.img_size)
-tex  = np.array(tex ) 
+tex  = np.array(tex ) * facial_seg
 tex = cv2.cvtColor(tex, cv2.COLOR_RGB2BGR)
 # tex = cv2.imread(tex_path)
 # tex = cv2.resize(tex, self.img_size, interpolation = cv2.INTER_AREA)
