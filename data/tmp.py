c37d6cbfbc3821ae724f0bf3be59c63c1d563434
Lele Chen <lelechen@fb.com>
2021-05-14 16:04:17 -0400

    g

--- data/tmp.py
+++ data/tmp.py
@@ -13,6 +13,8 @@ tex_path = '/raid/celong/FaceScape/textured_meshes/1/models_reg/16_grin.jpg'
 # mesh 
 tex = Image.open(tex_path).convert('RGB')#.resize(self.img_size)
 tex  = np.array(tex ) * facial_seg
+tex = tex.astype(int)
+print (np.unique(tex))
 tex = cv2.cvtColor(tex.astype(int), cv2.COLOR_RGB2BGR)
 # tex = cv2.imread(tex_path)
 # tex = cv2.resize(tex, self.img_size, interpolation = cv2.INTER_AREA)
