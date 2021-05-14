af68f212a762b128292ad383a0b4314b9b6e802b
Lele Chen <lelechen@fb.com>
2021-05-14 15:53:54 -0400

    g

--- data/tmp.py
+++ data/tmp.py
@@ -1,10 +1,13 @@
 from PIL import Image, ImageChops, ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 import PIL
+import cv2
 tex_path = '/raid/celong/FaceScape/textured_meshes/1/models_reg/16_grin.jpg'
 # mesh 
 tex = Image.open(tex_path).convert('RGB')#.resize(self.img_size)
 tex  = np.array(tex ) 
+tex = cv2.cvtColor(im_cv, cv2.COLOR_RGB2BGR)
+
 # tex = cv2.resize(tex, self.img_size, interpolation = cv2.INTER_AREA)
 # tex = tex * self.facial_seg
 cv2.imwrite('./gg.png', tex)
