56cf0d971b4e75caff1120f1ff7ba9767ba6bded
Lele Chen <lelechen@fb.com>
2021-05-14 22:37:24 -0400

    g

--- data/tmp.py
+++ data/tmp.py
@@ -32,7 +32,7 @@ tex =  np.uint8(tex)
 print (np.unique(tex))
 print (tex.shape)
 l = max(w, h)
-tex = tex[y:y+l,x:x+l,:]
+tex = tex[y:y+l,x- (l-w)/2:x+l,:]
 tex = cv2.cvtColor(tex, cv2.COLOR_RGB2BGR)
 
 # tex = cv2.imread(tex_path)
