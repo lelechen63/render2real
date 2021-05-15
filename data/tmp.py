33c3e00d4bdeed959edcff92f01aad9320fda922
Lele Chen <lelechen@fb.com>
2021-05-14 22:34:55 -0400

    g

--- data/tmp.py
+++ data/tmp.py
@@ -6,7 +6,15 @@ import numpy as np
 
 facial_seg = Image.open("./predef/facial_mask_v10.png")
 facial_seg  = np.array(facial_seg )
-
+kk = cv2.imread("./predef/facial_mask_v10.png")
+gray = cv2.cvtColor(kk, cv2.COLOR_BGR2GRAY)
+edged = cv2.Canny(gray, 30, 200)
+cnts = cv2.findContours(edged, cv2.RETR_EXTERNAL,
+                cv2.CHAIN_APPROX_SIMPLE)
+cnts = cv2.findContours(edged, 
+    cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+cnts = cnts[0]
+x,y,w,h = cv2.boundingRect(cnts)
 
 print (x,y,w, h)
 
@@ -23,8 +31,7 @@ tex  = np.array(tex ) * facial_seg
 tex =  np.uint8(tex)
 print (np.unique(tex))
 print (tex.shape)
-
-tex = tex[y:y+max(h, w),x:x+max(h,w),:]
+tex = tex[y:y+max(h,w),x:x+max(h,w),:]
 tex = cv2.cvtColor(tex, cv2.COLOR_RGB2BGR)
 
 # tex = cv2.imread(tex_path)
