2915c741eb955035f65cfa4b2a27a0634be663f6
Lele Chen <lelechen@fb.com>
2021-05-25 19:31:10 -0400

    g

--- data/tmp.py
+++ data/tmp.py
@@ -24,7 +24,7 @@ facial_seg = np.expand_dims(facial_seg, axis=2)
 print (np.unique(facial_seg))
 
 
-tex_path = '/raid/celong/FaceScape/textured_meshes/1/models_reg/16_grin.jpg'
+tex_path = '/raid/celong/FaceScape/texture_mapping/target/1/9_mouth_right.png'
 # mesh 
 tex = Image.open(tex_path).convert('RGB')#.resize(self.img_size)
 tex  = np.array(tex ) * facial_seg
