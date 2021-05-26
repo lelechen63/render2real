ee501113308d158dca159821f9570d3750b098b5
Lele Chen <lelechen@fb.com>
2021-05-25 21:54:13 -0400

    g

--- models/networks.py
+++ models/networks.py
@@ -187,7 +187,6 @@ class VGGLoss(nn.Module):
         self.weights = [1.0/32, 1.0/16, 1.0/8, 1.0/4, 1.0]        
 
     def forward(self, x, y):
-        print (x.shape, y.shape,' ==+++++++')           
         x_vgg, y_vgg = self.vgg(x), self.vgg(y)
         loss = 0
         for i in range(len(x_vgg)):
