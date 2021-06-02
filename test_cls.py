32b27c1b135790f96b470a4872d304ec03167857
Lele Chen <lelechen@fb.com>
2021-06-02 16:48:48 -0400

    g

--- test_cls.py
+++ test_cls.py
@@ -52,10 +52,10 @@ dataset_size = len(data_loader)
 print('#testing images = %d' % dataset_size)
 if opt.clsname == 'idcls':
     cls = networks.TexClassifier(opt.loadSize, 301, 64, opt.n_downsample_global, opt.n_blocks_global)
-    cls.load_state_dict(torch.load('/raid/celong/lele/github/render2real/checkpoints/cls/100_net_idcls.pth'))
+    cls.load_state_dict(torch.load('/raid/celong/lele/github/render2real/checkpoints/cls/200_net_idcls.pth'))
 else:
     cls = networks.TexClassifier(opt.loadSize, 20, 64, opt.n_downsample_global, opt.n_blocks_global)
-    cls.load_state_dict(torch.load('/raid/celong/lele/github/render2real/checkpoints/cls/100_net_expcls.pth'))
+    cls.load_state_dict(torch.load('/raid/celong/lele/github/render2real/checkpoints/cls/200_net_expcls.pth'))
 cls = cls.cuda()
 criterion = torch.nn.CrossEntropyLoss()
 
