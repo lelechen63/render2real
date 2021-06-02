0a98d71120f74180b8963dc86359484872eba32b
Lele Chen <lelechen@fb.com>
2021-06-02 16:44:05 -0400

    g

--- test_cls.py
+++ test_cls.py
@@ -73,7 +73,8 @@ for i, data in enumerate(dataset):
     out_labels = cls( data['tex'].cuda())
     print(out_labels.shape)
     print(gt_lbs)
-    loss = criterion(out_labels, gt_lbs.cuda())
+    gt_lbs = gt_lbs.cuda()
+    loss = criterion(out_labels, gt_lbs)
     loss = loss.mean()
     # calculate final loss scalar
     ############### Backward Pass ####################
