baae3a18d3862dc40cda28299698d08b83326d13
Lele Chen <lelechen@fb.com>
2021-06-02 15:42:39 -0400

    g

--- scripts/train_512p.sh
+++ scripts/train_512p.sh
@@ -1,2 +1,2 @@
 ### Using labels only
- CUDA_VISIBLE_DEVICES=6,7,5,4,3,2,1,0  python step1_texmesh_train.py --datasetname fs_texmesh --model texmesh_step1 --gpu_ids 0,1,2,3,4,5,6,7 --batchSize 32
+ CUDA_VISIBLE_DEVICES=6,7,5,4,3,2,1,0  python step1_texmesh_train.py --datasetname fs_texmesh --name texmesh_step1 --gpu_ids 0,1,2,3,4,5,6,7 --batchSize 32
